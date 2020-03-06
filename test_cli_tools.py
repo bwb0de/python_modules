aa55414742e7aa64191a6439e13c0b399bf0aca0
Daniel Cruz <danielc@unb.br>
2020-03-06 19:25:07 -0300

    Apagando teste desnecessário

--- test_cli_tools.py
+++ test_cli_tools.py
@@ -196,15 +196,6 @@ def test_create_column_metainfo_file():
 
 
 
-def test_print_text_table_file():
-    with open('text_table_file.test', 'w') as f:
-        f.write(table5)
-    print_text_table_file('text_table_file.test', delimitor=':')
-    print_text_table_file('text_table_file.test', delimitor=':', count_lines=False)
-    os.remove('text_table_file.test')
-
-
-
 def test_return_bisect_lists():
     assert return_bisect_lists([1,2,3,4,5,6,7,8,9]) == ([1,2,3,4], 5, [6,7,8,9])
     assert return_bisect_lists([1,2,3,4,5,6,7,8]) == ([1,2,3,4], 5, [6,7,8])
